ea2d9cf571cb48ea4a9bd863f50a8094f3c3a28b
elsaad92 <99730108+elsaad92@users.noreply.github.com>
2024-03-25 17:47:03 +0300

    this HMS with mortasofa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+// Import the NextConfig type for TypeScript support. Remove if not using TypeScript
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextTranslate = require("next-translate");
 
-export default nextConfig;
+const nextConfig = {
+  // Your existing Next.js configuration options here
+
+  // Add the i18n configuration with correct locale settings
+  i18n: {
+    locales: ["defaultLocale", "otherLocale"], // Specify your actual locales here, e.g., ["en", "ar"]
+    defaultLocale: "defaultLocale", // Set your actual default locale, e.g., "en"
+  },
+
+  // Any additional Next.js configurations you have
+};
+
+// Now wrap the nextConfig with nextTranslate and export it
+module.exports = nextTranslate(nextConfig);
